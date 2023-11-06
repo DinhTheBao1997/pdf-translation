646e924c183c4216a105198d44cf76d1849b3f23
BaoDT <baodt@imlink.vn>
2023-11-06 17:00:49 +0700

    Fine tune

--- training-model/fine-tune.py
+++ training-model/fine-tune.py
@@ -1,6 +1,6 @@
-from transformers import MBartForConditionalGeneration, MBart50TokenizerFast
-model = MBartForConditionalGeneration.from_pretrained("facebook/mbart-large-50-many-to-many-mmt")
-tokenizer = MBart50TokenizerFast.from_pretrained("facebook/mbart-large-50-many-to-many-mmt", src_lang="en_XX")
+from transformers import AutoTokenizer, BartModel
+model = AutoTokenizer.from_pretrained("facebook/bart-base")
+tokenizer = BartModel.from_pretrained("facebook/bart-base")
 text = "Bhutan begins biggest vaccination drive against Covid-19"
 model_inputs = tokenizer(text, return_tensors = "pt")
 generated_tokens = model.generate(**model_inputs, forced_bos_token_id = tokenizer.lang_code_to_id ["vi_VN"])
