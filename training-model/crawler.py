8e19ba902bdba0551dd534ae9de324427679917c
BaoDT <baodt@imlink.vn>
2023-11-21 12:01:12 +0700

    Auto commit

--- training-model/crawler.py
+++ training-model/crawler.py
@@ -3,53 +3,65 @@ from bs4 import BeautifulSoup
 import csv
 from variable import train_urls, test_urls
 import hashlib
+import json
 
-train_mode = True
-result = {}
 
-if train_mode:
-    file_name = "train"
-    urls = train_urls
-else:
-    file_name = "test"
-    urls = test_urls
+def crawler(train_mode):
+    if train_mode:
+        file_name = "train"
+        urls = train_urls
+    else:
+        file_name = "test"
+        urls = test_urls
 
-file_path = f'data/{file_name}.csv'
-with open(file_path, 'w', encoding="utf-8") as file:
-    writer = csv.writer(file, lineterminator="\n")
-    writer.writerow(["en", "ja", "vi"])
+    queries = [
+        ".para .topic__explanation",
+        ".topic__header--subsection",
+        ".para>p"
+    ]
+    def extract_text(elements, lst: list[str]):
+        for element in elements:
+            lst.append(element.text.strip().replace("’", "'"))
 
-origin = "https://www.msdmanuals.com"
-def crawler(url, mode: str):
-    texts = []
-    response = requests.get(url[mode])
-    text = response.text
+    def crawler(url, mode: str):
+        texts = []
+        response = requests.get(url[mode])
+        text = response.text
 
-    # Parse the HTML content
-    soup = BeautifulSoup(text, "html.parser")
-    nodeList = soup.find_all(class_="para")
-    for node in nodeList:
-        text: str = node.text
-        texts.append(text.strip())
-    result[mode] = texts
+        # Parse the HTML content
+        soup = BeautifulSoup(text, "html.parser")
+        for query in queries:
+            elements = soup.select(query)
+            extract_text(elements, texts)
+        return texts
 
-def save_csv():
-    texts_en = result["en"]
-    texts_vi = result["vi"]
-    texts_ja = result["ja"]
-    if len(texts_en) != len(texts_vi):
-        return
-    with open(file_path, 'a', encoding="utf-8") as file:
-        writer = csv.writer(file,lineterminator="\n")
-        for i in range(0, len(texts_en)):
-            writer.writerow([texts_en[i], texts_ja[i], texts_vi[i]])
 
-# Run the crawler function
-for url in urls:
-    crawler(url, "en")
-    crawler(url, "ja")
-    crawler(url, "vi")
-    try:
-        save_csv()
-    except:
-        print(url)
+    file_path = f'data/{file_name}.csv'
+    with open(file_path, 'w', encoding="utf-8") as file:
+        writer = csv.writer(file, lineterminator="\n")
+        writer.writerow(["id", "translation", "url"])
+
+    def save_csv(texts_en: list[str], texts_vi: list[str], texts_ja: list[str], url):
+        length = min(len(texts_en), len(texts_vi), len(texts_ja))
+        with open(file_path, 'a', encoding="utf-8") as file:
+            writer = csv.writer(file,lineterminator="\n")
+            for i in range(0, length):
+                dic = {
+                    'en': texts_en[i],
+                    'vi': texts_vi[i],
+                    'ja': texts_ja[i],
+                }
+                text = json.dumps(dic, ensure_ascii=False)
+                index = hashlib.md5(url.encode()).hexdigest()
+                print([index, text, url])
+                writer.writerow([index, text, url])
+
+    # Run the crawler function
+    for url in urls:
+        texts_en = crawler(url, "en")
+        texts_vi = crawler(url, "vi")
+        texts_ja = crawler(url, "ja")
+        save_csv(texts_en, texts_vi, texts_ja, url["en"])
+
+for mode in [True, False]:
+    crawler(mode)
--- training-model/scripts.py
+++ training-model/scripts.py
@@ -1,9 +1,35 @@
-from datasets import load_dataset, DatasetDict
-import pandas as pd
 
-# Source: https://huggingface.co/datasets/alt
-dataset: DatasetDict = load_dataset('alt')
-df = pd.DataFrame(dataset["train"])  # Assuming "train" is a split in your dataset
+import json
+import csv
+source_file_path = 'data/train.en'
+target_file_path = 'data/train.vi'
 
-# Save the DataFrame to a CSV file
-df.to_csv("output_file.csv", index=False)
+# Read the contents of source and target files
+with open(source_file_path, 'r', encoding='utf-8') as source_file:
+    source_lines = source_file.readlines()
+
+with open(target_file_path, 'r', encoding='utf-8') as target_file:
+    target_lines = target_file.readlines()
+
+# Remove newline characters and create a list of pairs
+dataset = list(zip(source_lines, target_lines))
+
+file_name = "en-vi"
+file_path = f'data/{file_name}.csv'
+with open(file_path, 'w', encoding="utf-8") as file:
+    writer = csv.writer(file, lineterminator="\n")
+    writer.writerow(["id", "translation"])
+
+# Display the resulting dataset
+i = 1
+with open(file_path, 'a', encoding="utf-8") as file:
+    writer = csv.writer(file,lineterminator="\n")
+    for pair in dataset:
+        source_text, target_text = pair
+        dic = {
+            'en': source_text.strip(),
+            'vi': target_text.strip(),
+        }
+        text = json.dumps(dic, ensure_ascii=False)
+        writer.writerow([i, text])
+        i += 1
